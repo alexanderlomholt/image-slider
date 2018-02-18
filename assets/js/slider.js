a9b462c1a2927034e042added7e17f0dcd38bbe6
Alexander Lomholt <arlomholt@gmail.com>
2018-02-17 23:25:00 -0500

    creted layout of the slider

---
+++ assets/js/slider.js
@@ -0,0 +1,24 @@
+// array of images
+const images = ['image1.jpeg','','','',''];
+
+
+// '../../../images/'
+
+const leftArrow = document.querySelector('#left');
+const rightArrow = document.querySelector('#right');
+
+leftArrow.addEventListener('click', (e) => {
+  console.log('Left arrow triggered');
+});
+
+rightArrow.addEventListener('click', (e) => {
+  console.log('Right arrow triggered');
+});
+
+
+
+
+
+
+
+
